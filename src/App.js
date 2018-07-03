2fcac9f059d5836d0c15aac52f05e3bb7c7cb955
NickWalty <nick.bell@gmail.com>
2018-07-03 16:45:20 -0300

    Adding files to Heroku app

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ displayFaceBox = (box) => {
 
 onButtonSubmit = () => {
  this.setState({imageURL: this.state.input});
-  fetch('http://localhost:3000/imageurl', {
+  fetch('https://shrouded-springs-44192.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -100,7 +100,7 @@ onButtonSubmit = () => {
   .then(response => response.json())
     .then(response => {
       if(response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://shrouded-springs-44192.herokuapp.com/image', {
         method: 'put',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
